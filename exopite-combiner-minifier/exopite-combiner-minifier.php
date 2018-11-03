f63e2a88014c4422c41fb5245deaefd9c1839f67
Joe <ujbeszel@gmail.com>
2018-11-03 12:07:05 +0100

    Add function to show upgrade notification

--- exopite-combiner-minifier/exopite-combiner-minifier.php
+++ exopite-combiner-minifier/exopite-combiner-minifier.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Exopite Combiner and Minifier
  * Plugin URI:        https://www.joeszalai.org/exopite/exopite-combiner-minifier
  * Description:       Minify and Combine Javascripts and CSS resources for better SEO and page speed. jQuery and external resources will be ignored.
- * Version:           20180817
+ * Version:           20181103
  * Author:            Joe Szalai
  * Author URI:        https://www.joeszalai.org
  * License:           GPL-2.0+
@@ -87,7 +87,7 @@ define( 'EXOPITE_COMBINER_MINIFIER_PLUGIN_NAME', 'exopite-combiner-minifier' );
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'EXOPITE_COMBINER_MINIFIER_VERSION', '20180817' );
+define( 'EXOPITE_COMBINER_MINIFIER_VERSION', '20181103' );
 
 /**
  * The code that runs during plugin activation.
@@ -137,6 +137,30 @@ if ( is_admin() ) {
         __FILE__, //Full path to the main plugin file.
         EXOPITE_COMBINER_MINIFIER_PLUGIN_NAME //Plugin slug. Usually it's the same as the name of the directory.
     );
+
+	/**
+	 * add plugin upgrade notification
+	 * https://andidittrich.de/2015/05/howto-upgrade-notice-for-wordpress-plugins.html
+	 */
+	add_action( 'in_plugin_update_message-' . EXOPITE_COMBINER_MINIFIER_PLUGIN_NAME . '/' . EXOPITE_COMBINER_MINIFIER_PLUGIN_NAME .'.php', 'exopite_combiner_minifier_show_upgrade_notification', 10, 2 );
+	function exopite_combiner_minifier_show_upgrade_notification( $current_plugin_metadata, $new_plugin_metadata ) {
+
+		/**
+		 * Check "upgrade_notice" in readme.txt.
+		 *
+		 * Eg.:
+		 * == Upgrade Notice ==
+		 * = 20180624 = <- new version
+		 * Notice		<- message
+		 *
+		 */
+		if ( isset( $new_plugin_metadata->upgrade_notice ) && strlen( trim( $new_plugin_metadata->upgrade_notice ) ) > 0 ) {
+
+			// Display "upgrade_notice".
+			echo sprintf( '<span style="background-color:#d54e21;padding:10px;color:#f9f9f9;margin-top:10px;display:block;"><strong>%1$s: </strong>%2$s</span>', esc_attr( 'Important Upgrade Notice', 'exopite-multifilter' ), esc_html( rtrim( $new_plugin_metadata->upgrade_notice ) ) );
+
+		}
+    }
 
 }
 // End Update
