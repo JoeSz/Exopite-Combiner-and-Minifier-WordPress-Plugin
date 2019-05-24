930c4e8facb6c1b6cde550d9b95d10bdd54cf1bb
Joe <ujbeszel@gmail.com>
2019-05-24 07:27:08 +0200

    Update version

--- exopite-combiner-minifier/exopite-combiner-minifier.php
+++ exopite-combiner-minifier/exopite-combiner-minifier.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Exopite Combiner and Minifier
  * Plugin URI:        https://www.joeszalai.org/exopite/exopite-combiner-minifier
  * Description:       Minify and Combine Javascripts and CSS resources for better SEO and page speed. jQuery and external resources will be ignored.
- * Version:           20190213
+ * Version:           20190521
  * Author:            Joe Szalai
  * Author URI:        https://www.joeszalai.org
  * License:           GPL-2.0+
@@ -87,7 +87,7 @@ define( 'EXOPITE_COMBINER_MINIFIER_PLUGIN_NAME', 'exopite-combiner-minifier' );
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'EXOPITE_COMBINER_MINIFIER_VERSION', '20190213' );
+define( 'EXOPITE_COMBINER_MINIFIER_VERSION', '20190521' );
 
 /**
  * The code that runs during plugin activation.
