e7cfbdd7d0ee69c2f41788a0c11a0eb3c601d9b2
JoeSz <ujbeszel@gmail.com>
2020-04-28 19:36:02 +0200

    Remove unused functions

--- exopite-combiner-minifier/public/class-exopite-combiner-minifier-public.php
+++ exopite-combiner-minifier/public/class-exopite-combiner-minifier-public.php
@@ -60,7 +60,6 @@ class Exopite_Combiner_Minifier_Public {
 
     public $regex_js_template = '/<script(.*?)>([\s\S]*?)<\/script>/i';
 
-
 	/**
 	 * Initialize the class and set its properties.
 	 *
@@ -75,52 +74,6 @@ class Exopite_Combiner_Minifier_Public {
         $this->debug = false;
         $this->showinfo = false;
         // $this->showinfo = true;
-
-	}
-
-	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Exopite_Combiner_Minifier_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Exopite_Combiner_Minifier_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		// wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/exopite-combiner-minifier-public.css', array(), $this->version, 'all' );
-
-	}
-
-	/**
-	 * Register the JavaScript for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Exopite_Combiner_Minifier_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Exopite_Combiner_Minifier_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		// wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/exopite-combiner-minifier-public.js', array( 'jquery' ), $this->version, false );
 
 	}
 
